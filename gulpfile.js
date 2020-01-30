b7d2e18ee15f94b93d41f533f3e48e233a321706
Diogen <lahentum@gmail.com>
2020-01-30 15:42:57 +0200

    gulp builder v.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-const gulp = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-gulp.task('server', function() {
+function stream() {
 	browserSync.init({
 		server: {
 			baseDir: "./src/"
 		}
 	});
-});
+	watch('./src/sass/**/*.sass', styles);
+	watch('./src/*.html').on('change', browserSync.reload);
+}
 
-gulp.task('watch', function() {
-	gulp.watch('./src/*.html').on('change', browserSync.reload);
-	gulp.watch('./src/sass/**/*.sass', gulp.parallel('sass'));
-});
-
-gulp.task('sass', function(){
-	return gulp.src('./src/sass/style.sass')
+function styles(){
+	return src('./src/sass/style.sass')
 		.pipe(sass({indentType: 'tab', indentWidth: 1, outputStyle: 'expanded'}))
-		.pipe(gulp.dest('./src/css/'))
+		.pipe(dest('./src/css/'))
 		.pipe(browserSync.stream());
-});
+}
 
-gulp.task('default', gulp.series('sass', gulp.parallel('server', 'watch')));
+exports.styles = styles;
+exports.stream = stream;
+
+exports.default = series(parallel(styles), stream);
